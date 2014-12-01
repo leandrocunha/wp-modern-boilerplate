146f497976453706a3a3e374d611be807e23fbe0
Leandro Cunha aka. Frango <leandroscunha@gmail.com>
2014-12-01 16:11:40 -0200

    fix navbbar when logged

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Project configuration.
   grunt.initConfig({
@@ -11,24 +12,6 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
-      options: {
-        banner: '/*\n' +
-                'Theme Name: WP Modern Boilerplate\n' +
-                'Theme URI: https://github.com/leandrocunha/wp-modern-boilerplate\n' +
-                'Author: Leandro Cunha aka. Frango\n' +
-                'Author URI: http://frango.herokuapp.com\n' +
-                'Description: The modern boilerplate to build WordPress themes more powerfull\n' +
-                'Version: 1.0\n' +
-                'License: GNU General Public License v2 or later\n' +
-                'License URI: http://www.gnu.org/licenses/gpl-2.0.html\n' +
-                'Tags: gray, blue, white, flat, bootstrap, grunt, bower\n' +
-                'Text Domain: wpmodernboilerplate\n' +
-                '\n' +
-                'This theme, like WordPress, is licensed under the GPL.\n' +
-                'Use it to make something cool, have fun, and share what you\'ve learned with others.\n' +
-                '*/',
-      },
-
       dist: {
         files: {
           'style.css': 'sass/app.scss'
@@ -67,16 +50,42 @@ module.exports = function(grunt) {
       },
     },
 
+    cssmin: {
+      minify: {
+        options: {
+          banner: '/*\n' +
+                  'Theme Name: WP Modern Boilerplate\n' +
+                  'Theme URI: https://github.com/leandrocunha/wp-modern-boilerplate\n' +
+                  'Author: Leandro Cunha aka. Frango\n' +
+                  'Author URI: http://frango.herokuapp.com\n' +
+                  'Description: The modern boilerplate to build WordPress themes more powerfull\n' +
+                  'Version: 1.0\n' +
+                  'License: GNU General Public License v2 or later\n' +
+                  'License URI: http://www.gnu.org/licenses/gpl-2.0.html\n' +
+                  'Tags: gray, blue, white, flat, bootstrap, grunt, bower\n' +
+                  'Text Domain: wpmodernboilerplate\n' +
+                  '\n' +
+                  'This theme, like WordPress, is licensed under the GPL.\n' +
+                  'Use it to make something cool, have fun, and share what you\'ve learned with others.\n' +
+                  '*/\n',
+        },
+
+        files: {
+          'style.css': ['style.css']
+        }
+      }
+    },
+
     watch: {
       scripts: {
-        files: ['sass/*.scss'],
-        tasks: ['sass']
+        files: ['sass/*.sass', 'sass/*.scss', 'fonts/*.scss'],
+        tasks: ['sass', 'cssmin']
       },
     }
 
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'copy', 'watch']);
+  grunt.registerTask('default', ['sass', 'copy', 'cssmin', 'watch']);
 
 };
