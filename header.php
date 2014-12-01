130231017eeb63b6fe3817c7d32bc8422a4fb6a2
Leandro Cunha aka. Frango <leandroscunha@gmail.com>
2014-12-01 12:47:08 -0200

    Added support to IE8

--- header.php
+++ header.php
@@ -13,7 +13,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<!--[if lt IE 9]>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 		<?php wp_head(); ?>
 
